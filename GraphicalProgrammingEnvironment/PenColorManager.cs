da7d00b5981d54ad917b35cf662ae73b3016e3e0
yurikajoshi <yukayuva@gmail.com>
2023-11-13 07:39:16 +0000

    updated xml documentation comments for PenColorManager

--- GraphicalProgrammingEnvironment/PenColorManager.cs
+++ GraphicalProgrammingEnvironment/PenColorManager.cs
@@ -8,17 +8,28 @@ using System.Windows.Forms;
 
 namespace GraphicalProgrammingEnvironment
 {
+    /// <summary>
+    /// Manages the pen color for drawing in the graphical programming environment.
+    /// </summary>
     public class PenColorManager
     {
         private Form1 formInstance;
         private Pen currentPen;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PenColorManager"/> class.
+        /// </summary>
+        /// <param name="form">The main form instance.</param>
         public PenColorManager(Form1 form)
         {
             formInstance = form;
             currentPen = new Pen(Color.Black, 2); // Default pen color is black
         }
 
+        /// <summary>
+        /// Processes the 'pen' command, setting the current pen color.
+        /// </summary>
+        /// <param name="command">The command string, e.g., 'pen Red'.</param>
         public void ProcessPenCommand(string command)
         {
             if (command.StartsWith("pen ", StringComparison.OrdinalIgnoreCase))
@@ -45,6 +56,10 @@ namespace GraphicalProgrammingEnvironment
             }
         }
 
+        /// <summary>
+        /// Gets the current pen used for drawing.
+        /// </summary>
+        /// <returns>The current pen.</returns>
         public Pen GetCurrentPen()
         {
             return currentPen;
