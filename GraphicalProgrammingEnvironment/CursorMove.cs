9495dd9c4a183f259b245f86e797845aee594190
yurikajoshi <yukayuva@gmail.com>
2023-11-13 07:27:58 +0000

    updated xml documentation comments for CursorMove cs that handles the moveto command

--- GraphicalProgrammingEnvironment/CursorMove.cs
+++ GraphicalProgrammingEnvironment/CursorMove.cs
@@ -8,35 +8,61 @@ using System.Windows.Forms;
 
 namespace GraphicalProgrammingEnvironment
 {
+    /// <summary>
+    /// Handles the movement of the cursor in the graphical programming environment.
+    /// </summary>
     public class CursorMove
     {
         private Form1 formInstance; // Instance of Form1
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CursorMove"/> class.
+        /// </summary>
+        /// <param name="form">The instance of the main form (Form1).</param>
         public CursorMove(Form1 form)
         {
-            formInstance = form; // Pass the current instance of Form1 to the MoveCursor class
+            formInstance = form; // Passes the current instance of Form1 to the MoveCursor class
         }
+
+        /// <summary>
+        /// Processes the 'moveTo' command, updating the cursor position on the form.
+        /// </summary>
+        /// <param name="commandPart">An array containing the command parts.</param>
         public void ProcessMoveToCommand(string[] commandPart)
         {
-            if (commandPart.Length == 3 && int.TryParse(commandPart[1], out int x) && int.TryParse(commandPart[2], out int y))
+            try
             {
-                formInstance.cursorPosition = new Point(x, y); // Updates the cursor position
-                formInstance.RefreshPictureBox(); // Refresh the picture box
-            }
-            else if (commandPart.Length == 2 && commandPart[1].Contains(','))
-            {
-                string[] commandCoordinate = commandPart[1].Split(',');
-                if (int.TryParse(commandCoordinate[0], out int a) && int.TryParse(commandCoordinate[1], out int b))
+                if (commandPart.Length == 3 && int.TryParse(commandPart[1], out int x) && int.TryParse(commandPart[2], out int y))
                 {
-                    formInstance.cursorPosition = new Point(a, b); // Updates the cursor position in the current Form1 instance
+                    // Updates cursor position with specified coordinates
+                    formInstance.cursorPosition = new Point(x, y);
+                    // Refreshes the PictureBox to reflect the new cursor position
                     formInstance.RefreshPictureBox();
                 }
+                else if (commandPart.Length == 2 && commandPart[1].Contains(','))
+                {
+                    // Parse coordinates separated by a comma
+                    string[] commandCoordinate = commandPart[1].Split(',');
+                    if (int.TryParse(commandCoordinate[0], out int a) && int.TryParse(commandCoordinate[1], out int b))
+                    {
+                        // Updates cursor position with parsed coordinates
+                        formInstance.cursorPosition = new Point(a, b);
+                        // Refreshes the PictureBox to reflect the new cursor position
+                        formInstance.RefreshPictureBox();
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid 'moveTo' command. Please make sure to type 'moveTo x-coordinate y-coordinate'.");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Invalid 'moveTo' command. Please make sure to type 'moveTo x-coordinate y-coordinate'.");
+                // Handles the exception (display an error message, log the exception, etc.)
+                MessageBox.Show($"Error: {ex.Message}");
             }
         }
+
 
 
     }
